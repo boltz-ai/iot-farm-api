02173b43e22a28d6417b43e7621a9ab8f811d47c
Đỗ Ngọc Ngoạn <ngocngoan060288@gmail.com>
2020-03-09 15:04:22 +0700

    add secondary database setting

--- iot_farm/iot_farm/settings.py
+++ iot_farm/iot_farm/settings.py
@@ -95,12 +95,20 @@ WSGI_APPLICATION = 'iot_farm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get("DATABASE_ENGINE", "django.db.backends.sqlite3"),
+        'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.environ.get('DATABASE_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+    },
+    'secondary': {
+        'ENGINE': os.environ.get('DATABASE_SECONDARY_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DATABASE_SECONDARY_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'HOST': os.environ.get('DATABASE_SECONDARY_HOST'),
+        'PORT': os.environ.get('DATABASE_SECONDARY_PORT'),
+        'USER': os.environ.get('DATABASE_SECONDARY_USER'),
+        'PASSWORD': os.environ.get('DATABASE_SECONDARY_PASSWORD'),
     },
     'iot': {
         'ENGINE': 'djongo',
